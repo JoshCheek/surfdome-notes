6ed079cda0f878f04e9a79e400eb521ff40094d1
Josh Cheek <josh.cheek@gmail.com>
2014-01-29 09:25:22 +0000

    Add more notes from 2014-01-28

--- 2014-01-28.rb
+++ 2014-01-28.rb
@@ -1,7 +1,7 @@
 # how does method(&:something) work?
 
 [*1..10].map(&:even?)  # => [false, true, false, true, false, true, false, true, false, true]
-:even?.to_proc         # => #<Proc:0x007fe935164970>
+:even?.to_proc         # => #<Proc:0x007fca641da8b8>
 
 # for symbols, to_proc looks like:
 class MySymbol
@@ -19,3 +19,29 @@ end
 :even?.to_proc.call 2  # => true
 2.send :even?          # => true
 2.even?                # => true
+
+
+# ERB
+
+shopping_cart = ['lamp', 'desk', 'pencils']  # => ["lamp", "desk", "pencils"]
+template = "
+<ul>
+  <% shopping_cart.each do |item| %>
+    <li><%= item %></li>
+  <% end %>
+</ul>
+"  # => "\n<ul>\n  <% shopping_cart.each do |item| %>\n    <li><%= item %></li>\n  <% end %>\n</ul>\n"
+
+require 'erb'                  # => true
+puts ERB.new(template).result  # => nil
+
+# >> 
+# >> <ul>
+# >>   
+# >>     <li>lamp</li>
+# >>   
+# >>     <li>desk</li>
+# >>   
+# >>     <li>pencils</li>
+# >>   
+# >> </ul>
